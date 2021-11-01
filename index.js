380236f584cedb27af8c040d0f1dd21ac97cc1e7
madison sorah <madisonsorah@gmail.com>
2021-11-01 13:29:05 -0400

    commit tree 5665dbb4634c1de54df6f75a85a0b3403841215e

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride > 2500) {
+    return 'No can do.';
+  } else if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride <= 400) {
+    return 'This one is on me!';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let cityResponse;
+  city === 'NYC' ? (cityResponse = 'Ok, sounds good.') : (cityResponse = 'No go.');
+  return cityResponse;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipResponse;
+  switch (tip) {
+    case 'generous':
+      tipResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipResponse = 'Thank you.';
+      break;
+    default:
+      tipResponse = 'Bye.';
+      break;
+  }
+  return tipResponse;
 }
